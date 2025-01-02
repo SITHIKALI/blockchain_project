1927d09a928f23472ec99ca20e4259a9d4dc5070
MOHAMAD SITHIK ALI F <135441194+SITHIKALI@users.noreply.github.com>
2025-01-02 16:22:37 +0000

    cn

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,14 @@
 // filepath: /workspaces/blockchain_project/scripts/deploy.js
 async function main() {
-  const HelloWorld = await ethers.getContractFactory("HelloWorld");
-  const hello = await HelloWorld.deploy();
-  await hello.deployed();
-  console.log("HelloWorld deployed to:", hello.address);
-}
-
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+    const hello = await HelloWorld.deploy();
+    await hello.deployed();
+    console.log("HelloWorld deployed to:", hello.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
