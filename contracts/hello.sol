f7875255cadab5d38e93052133a4082f45082731
MOHAMAD SITHIK ALI F <135441194+SITHIKALI@users.noreply.github.com>
2025-01-02 16:25:51 +0000

    cn

--- contracts/hello.sol
+++
@@ -1,7 +0,0 @@
-
-// filepath: /workspaces/blockchain_project/contracts/hello.sol
-pragma solidity ^0.8.0;
-
-contract HelloWorld {
-    string public greet = "Hello, World!";
-}
--- hello.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract HelloWorld {
-    string public greet = "Hello, World!";
-}
