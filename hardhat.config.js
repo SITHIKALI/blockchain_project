0abb54fe07890a512ff237da9e2c1406e74f6b53
MOHAMAD SITHIK ALI F <135441194+SITHIKALI@users.noreply.github.com>
2025-01-02 15:56:47 +0000

    new changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.0",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8546"
+    }
+  }
 };
